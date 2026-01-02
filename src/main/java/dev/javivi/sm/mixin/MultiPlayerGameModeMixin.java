e57d65af6910c151b292f6cbc19f51c4facc27d6
Javivi <128822513+javivi09dev@users.noreply.github.com>
2026-01-02 16:21:13 +0100

    add smoothbreak utilities

---
+++ src/main/java/dev/javivi/sm/mixin/MultiPlayerGameModeMixin.java
@@ -0,0 +1,22 @@
+package dev.javivi.sm.mixin;
+
+
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(MultiPlayerGameMode.class)
+public class MultiPlayerGameModeMixin {
+    @Shadow private ItemStack destroyingItem;
+
+
+    @ModifyVariable(method = "sameDestroyTarget", at = @At("STORE"))
+    private ItemStack replaceCachedItem(ItemStack itemStack) {
+        return this.destroyingItem;
+    }
+
+
+}
